3aa80a758f3d5b36b8a50a278b1513247b6b1fb7
alfatpololo <59309653+alfatpololo@users.noreply.github.com>
2022-09-25 13:10:44 +0700

    Update tugas3.js

--- tugas3.js
+++ tugas3.js
@@ -1,26 +1,22 @@
 const pilihNilai = (awal, akhir, array) => {
-    let hasil = [];
-    
-    array.sort((a, b) => a - b);
-    if (awal < akhir) {
-        if (array.length < 4) {
-            for (let i = 0; i < akhir; i++) {
-                if (array[i] >= awal && array[i] <= akhir) {
-                    hasil.push(array[i]);
-                }
-            }  
-            if (hasil.length > 0) {
-                console.log(hasil)
-            } else console.log("Hasil tidak ada");
-        } else console.log("Jumlah angka dalam dataArray harus lebih dari 5");
-    } else console.log("Nilai akhir harus lebih besar dari nilai awal");
-}
+  let hasil = [];
 
+  array.sort((a, b) => a - b);
+  if (awal < akhir) {
+    if (array.length >= 4) {
+      for (let i = 0; i < akhir; i++) {
+        if (array[i] >= awal && array[i] <= akhir) {
+          hasil.push(array[i]);
+        }
+      }
+      if (hasil.length > 0) {
+        console.log(hasil);
+      } else console.log("Hasil tidak ada");
+    } else console.log("Jumlah angka dalam array harus lebih dari 5");
+  } else console.log("nilai akhir harus lebih besar dari nilai awal");
+};
 
-pilihNilai(5, 20, [2, 25, 4, 14, 17, 30, 8]); // [ 8, 14, 17 ]
-
-pilihNilai(15, 3 , [2, 25, 4, 14, 17, 30, 8]); // “Nilai akhir harus lebih besar dari nilai awal”
-
-pilihNilai(5, 17 , [2, 25, 4]) // “Jumlah angka dalam dataArray harus lebih dari 5”
-
-pilihNilai(5, 17 , [2, 25, 4, 1, 30, 18]) // "Hasil tidak ada"
+pilihNilai(5, 20, [2, 25, 4, 14, 17, 30, 8]);
+pilihNilai(15, 3, [2, 25, 4, 14, 17, 30, 8]);
+pilihNilai(5, 17, [2, 25, 4]);
+pilihNilai(5, 17, [2, 25, 4, 1, 30, 18]);
